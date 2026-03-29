82e69d79f594062241ac747d05ca61f331d63828
uniplac-victorcunha <victor.cunha@uniplaclages.edu.br>
2026-03-29 19:07:13 -0300

    add: password hash security

--- src/routers/FuncionarioRouter.py
+++ src/routers/FuncionarioRouter.py
@@ -11,6 +11,7 @@ from domain.schemas.FuncionarioSchema import (
 # Infra
 from infra.orm.FuncionarioModel import FuncionarioDB
 from infra.database import get_db
+from infra.security import get_password_hash
 
 router = APIRouter()
 
@@ -53,6 +54,8 @@ async def post_funcionario(funcionario_data: FuncionarioCreate, db: Session = De
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Já existe um funcionário com este CPF"
             )
+        # Hash da senha
+        hashed_password = get_password_hash(funcionario_data.senha)
         # Cria o novo funcionário
         novo_funcionario = FuncionarioDB(
             id=None, # Será auto-incrementado
@@ -61,7 +64,7 @@ async def post_funcionario(funcionario_data: FuncionarioCreate, db: Session = De
             cpf=funcionario_data.cpf,
             telefone=funcionario_data.telefone,
             grupo=funcionario_data.grupo,
-            senha=funcionario_data.senha
+            senha=hashed_password
         )
         db.add(novo_funcionario)
         db.commit()
@@ -91,6 +94,9 @@ async def put_funcionario(id: int, funcionario_data: FuncionarioUpdate, db: Sess
                 raise HTTPException(
                     status_code=status.HTTP_400_BAD_REQUEST, detail="Já existe um funcionário com este CPF"
                 )
+        # Hash da senha se fornecida nova senha
+        if funcionario_data.senha:
+            funcionario_data.senha = get_password_hash(funcionario_data.senha)
         # Atualiza apenas os campos fornecidos
         update_data = funcionario_data.model_dump(exclude_unset=True)
         for field, value in update_data.items():
